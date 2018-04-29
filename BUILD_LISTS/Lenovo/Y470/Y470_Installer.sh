4c40c95f188f87f5835f92b33b22229d5390a4fd
Frank Wiggy <AttackofTheBranches@users.noreply.github.com>
2018-04-29 17:31:40 -0400

    Fully portable
    That's right

--- BUILD_LISTS/Lenovo/Y470/Y470_Installer.sh
+++ BUILD_LISTS/Lenovo/Y470/Y470_Installer.sh
@@ -1,14 +1,15 @@
+INSTALLDIR=$PWD
+diskutil mount /dev/disk0s1
 cd /Volumes/EFI/
 rm -rf EFI.bak
 cp -R EFI EFI.bak
-cd /MCUP/BUILD_LISTS/Lenovo/Y470 
+cd $PWD/BUILD_LISTS/Lenovo/Y470
 git clone https://github.com/Dwarven/Hackintosh.git Y470
 cd Y470
 git pull
 cd Lenovo\ Y470
 unzip kexts.zip
 cd kexts
-diskutil mount /dev/disk0s1
 mv * /Volumes/EFI/EFI/CLOVER/kexts/Other
 cp *.aml  /Volumes/EFI/EFI/CLOVER/ACPI/patched/
 cp config.plist /Volumes/EFI/EFI/CLOVER/config.plist
--- croninstall.sh
+++ croninstall.sh
@@ -1,4 +1,6 @@
+INSTALLDIR=$(dirname "$0")
+cd $INSTALLDIR
 crontab -l > HACK_UPDATE
-echo "0 0 * * TUE /MCUP/MacUpdater.sh" >> HACK_UPDATE
+echo "0 0 * * TUE $INSTALLDIR/MacUpdater.sh" >> HACK_UPDATE
 crontab HACK_UPDATE
 rm HACK_UPDATE
