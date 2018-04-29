ccfd9144d3de8e9d521ba7e01a0d0103f974dadf
Frank Wiggy <AttackofTheBranches@users.noreply.github.com>
2018-04-29 17:42:07 -0400

    Adding Documentation

--- Installer.sh
+++ Installer.sh
@@ -1,17 +1,21 @@
+# Getting working dir for script
 INSTALLDIR=$(dirname "$0")
+#Change to script's directory
 cd $INSTALLDIR
+#Display welcome screen
 echo "Welcome to The Hackintosh Updater"
-
+#Display computer families
 cat FAMILIES
-
+#Get user's family
 read -p "Please select your Computer Family: (# then RETURN) " FAM
 
+#Save computer file to config file
 sed "${FAM}q;d" FAMILIES_LIST > CURRENT_CONFIG/COMP_FAMILY
-
+#Load user's family
 COMP_FAMILY=`cat CURRENT_CONFIG/COMP_FAMILY`
 echo "Showing Builds for $COMP_FAMILY"
 
-
+#Print supported machine based on user input
 if [ $COMP_FAMILY = "Probook_Elitebook_Zbook" ]; then
 cat BUILD_LISTS/$COMP_FAMILY/COMP_BUILDS
 
@@ -20,11 +24,11 @@ read -p "Please select your build: (# then RETURN) " MODEL
 sed "${MODEL}q;d" BUILD_LISTS/$COMP_FAMILY/ACPI_CONFIG > CURRENT_CONFIG/COMP_MODEL
 
 COMP_MODEL=`cat CURRENT_CONFIG/COMP_MODEL`
-
+#Run config matcher
 BUILD_LISTS/Probook_Elitebook_Zbook/ACPI_TO_CONFIG.sh
 fi
 
-
+#Print supported machine based on user input
 if [ $COMP_FAMILY = "Lenovo" ]; then
 cat  /MCUP/BUILD_LISTS/$COMP_FAMILY/COMP_BUILDS
 echo ""
@@ -37,7 +41,7 @@ fi
 
 
 
-
+#Run updater
 echo "Installing for $COMP_MODEL"
 clear
 $INSTALLDIR/MacUpdater.sh
--- MacUpdater.sh
+++ MacUpdater.sh
@@ -1,17 +1,22 @@
+#Set working directory to script location
 INSTALLDIR=$(dirname "$0")
 cd $INSTALLDIR
+#Get config files
 COMP_FAMILY=`cat CURRENT_CONFIG/COMP_FAMILY`
 COMP_MODEL=`cat CURRENT_CONFIG/COMP_MODEL`
 echo "Running MCUP Update"
 echo "Selected Family is $COMP_FAMILY"
 echo "Selected Build is $COMP_MODEL"
 echo "Installing..."
-
+#Run Probook_Elitebook_Zbook installer
 if [ $COMP_FAMILY = "Probook_Elitebook_Zbook" ]; then
+  #Match ACPI to Clover Configs
 BUILD_LISTS/Probook_Elitebook_Zbook/ACPI_TO_CONFIG.sh
+#Run RehabInstaller
 BUILD_LISTS/Probook_Elitebook_Zbook/RehabInstaller.sh
 fi
-
+#Run if Lenovo
 if [ $COMP_FAMILY = "Lenovo" ]; then
+  #RUn lenovo grabber
 BUILD_LISTS/Lenovo/Lenovo_Install_Picker.sh
 fi
