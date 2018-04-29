70b5601b9b6fb042b0f1f4374d0b180cdd5952dd
Frank Wiggy <AttackofTheBranches@users.noreply.github.com>
2018-04-29 12:24:28 -0400

    DS remove

--- BUILD_LISTS/Probook_Elitebook_Zbook/RehabInstaller.sh
+++ BUILD_LISTS/Probook_Elitebook_Zbook/RehabInstaller.sh
@@ -1,4 +1,4 @@
-cd /MCUP/
+cd /MCUP/BUILD_LISTS/Probook_Elitebook_Zbook/
 COMP_FAMILY=`cat /MCUP/CURRENT_CONFIG/COMP_FAMILY`
 COMP_MODEL=`cat /MCUP/CURRENT_CONFIG/COMP_MODEL`
 COMP_CONFIG=`cat /MCUP/CURRENT_CONFIG/COMP_CONFIG`
@@ -15,7 +15,7 @@ cd Projects
 if [ ! -d "probook.git" ]; then
   git clone https://github.com/RehabMan/HP-ProBook-4x30s-DSDT-Patch probook.git
 fi
-cd /MCUP/Projects/probook.git/
+cd /MCUP/BUILD_LISTS/Probook_Elitebook_Zbook/Projects/probook.git/
 git pull
 ./download.sh
 ./install_downloads.sh
@@ -41,7 +41,7 @@ rm -rf EFI.bak
 cp -R EFI EFI.bak
 cd /Volumes/EFI/EFI/Clover/
 rm config.plist
-cd /MCUP/Projects/probook.git/
+cd /MCUP/BUILD_LISTS/Probook_Elitebook_Zbook/Projects/probook.git/
 
 cp ./config/$COMP_CONFIG /Volumes/EFI/EFI/Clover/config.plist
 fi
