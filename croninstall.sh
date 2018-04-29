a2f5625d887e2ca3dbd5a6eca75c768513fc9ef5
Frank Wiggy <AttackofTheBranches@users.noreply.github.com>
2018-04-29 12:38:25 -0400

    Set new Update time
    Every Tuesday

--- croninstall.sh
+++ croninstall.sh
@@ -1,4 +1,4 @@
-crontab -l > mycron
-echo "* * 25 * * /MCUP/MacUpdater.sh" >> mycron
-crontab mycron
-rm mycron
+crontab -l > HACK_UPDATE
+echo "0 0 * * TUE /MCUP/MacUpdater.sh" >> HACK_UPDATE
+crontab HACK_UPDATE
+rm HACK_UPDATE
