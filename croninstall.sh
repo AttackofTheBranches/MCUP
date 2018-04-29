30f244dae4bf2a6aff8218da29810d8e4f6841c7
Frank Wiggy <AttackofTheBranches@users.noreply.github.com>
2018-04-29 18:42:09 -0400

    Fixed Crontab installer
    Automated Updates are now working

--- croninstall.sh
+++ croninstall.sh
@@ -1,6 +1,11 @@
-INSTALLDIR=$(dirname "$0")
+#Set Installer dir
+INSTALLDIR=$(pwd)
 cd $INSTALLDIR
+# Make cron file
 crontab -l > HACK_UPDATE
-echo "0 0 * * TUE $INSTALLDIR/MacUpdater.sh" >> HACK_UPDATE
+# Echo update command into cron file
+sudo echo "0 0 * * TUE $INSTALLDIR/MacUpdater.sh" >> HACK_UPDATE
+# Install cron updater
 crontab HACK_UPDATE
+# Remove cron file
 rm HACK_UPDATE
