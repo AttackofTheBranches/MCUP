603527f654921906e784d76b56c0539e905a7ec6
Frank Wiggy <AttackofTheBranches@users.noreply.github.com>
2018-04-29 21:43:41 -0400

    Started Custom PC build script
    Not working, only the basics

---
+++ BUILD_LISTS/CUSTOM/Build_PC.sh
@@ -0,0 +1,25 @@
+INSTALLDIR=$PWD
+cd $INSTALLDIR
+echo "Custom Hackintosh Creation Kit"
+
+#GET BOARD FAMILY
+cat PARTS/MB_LIST
+#Get user's family
+read -p "Please select your Motherboard Family: (# then RETURN) " MB_TYPE
+
+#Save computer MB TYPE file to config file
+sed "${FAM}q;d" PARTS/MB_LIST > CUSTOM_CONFIG/MB_TYPE
+#Load user's family
+
+GPU_TYPE=`cat CUSTOM_CONFIG/GPU_TYPE`
+
+#GET GPU FAMILY
+cat PARTS/GPU_LIST
+#Get user's family
+read -p "Please select your GPU Family: (# then RETURN) " GPU_TYPE
+
+#Save computer file to config file
+sed "${FAM}q;d" PARTS/GPU > CUSTOM_CONFIG/GPU_TYPE
+#Load user's family
+
+GPU_TYPE=`cat CUSTOM_CONFIG/GPU_TYPE`
